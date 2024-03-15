19f71e5db99ded496d1175e85586787b6f06174c
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2024-03-15 10:34:11 +0530

    feat(): NavBar component added

---
+++ src/Components/ProcessAudio.jsx
@@ -0,0 +1,21 @@
+import NavBar from './NavBar'
+
+
+function ProcessAudio()
+{
+
+
+
+
+    
+    return (
+        <>
+<NavBar />
+        <h1 className="text-2xl">Process Audio</h1>
+{/* Process Audio 
+take input from the device (wav file ) pass to the api end point localhost:3000/lung-sound ,
+get json response and parse and display it 
+         */}
+        </>
+    )
+} export default ProcessAudio;

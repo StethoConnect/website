bda6792f7ca2d48f59e08f6fb9525096caf9c8b0
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2024-03-25 12:00:11 +0530

    fix(main.jsx):remove unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import {store} from './app/store.js'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-
-    <App />
-
-
-   
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
